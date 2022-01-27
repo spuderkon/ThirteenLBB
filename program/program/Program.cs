b116ed166f8e40f03ed332d805df42ea74c466a9
spuderrr <spuder000@gmail.com>
2022-01-27 20:51:10 +0500

    Deleted method from master cuz i don't know what happend

--- program/program/Program.cs
+++ program/program/Program.cs
@@ -30,25 +30,3 @@ switch (choice)
     default:
         break;
 }
-
-
-int addition(int a, int b)
-{
-    return a + b;
-}
-
-int subtraction(int a, int b)
-{
-    return a - b;
-}
-
-int multipication (int a, int b)
-{
-    return a * b;
-}
-
-double division (int a,int b)
-{
-    return a / b;
-}
-
